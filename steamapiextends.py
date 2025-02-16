3842c3571ae955bc4cc7270015241a8dfc2e35fe
ArjavTripathi <arjavtri@iastate.edu>
2025-02-16 11:49:47 -0600

    Latest, trying to get a list of names in json

--- steamapiextends.py
+++ steamapiextends.py
@@ -1,6 +1,7 @@
 from steam_web_api import Steam, Apps, Client, constants
 from requests import request, Response
 import json
+import regex
 import typing
 
 class extendsAPI(Apps):
@@ -16,7 +17,40 @@ class extendsAPI(Apps):
             self.__app_details_url,
             params={"appids": app_id, "cc": country, "filters": filters},
         )
+        split : list[str] = filters.split(',')
         json_loaded_response = json.loads(response.text)
+        if(json_loaded_response[f'{app_id}']['success']):
+            if('categories' in split):
+                try:
+                    json_loaded_response[f'{app_id}']['data']['categories'] = [category['description'] for category in json_loaded_response[f'{app_id}']['data']['categories']]
+                except:
+                    pass
+            if('genres' in split):
+                try:
+                    json_loaded_response[f'{app_id}']['data']['genres'] = [genres['description'] for genres in json_loaded_response[f'{app_id}']['data']['genres']]
+                except:
+                    pass
+            if('price_overview' in split):
+                try:
+                    json_loaded_response[f'{app_id}']['data']['price_overview'] = [{'currency': price['currency'], 'initial': price['initial']} for price in json_loaded_response[f'{app_id}']['data']['price_overview']]
+                except:
+                    pass
+            if('name' in split and len(split) == 1):
+                try:
+                    name = list(json_loaded_response.keys())[0]
+                    json_loaded_response[f'name'] = name
+                    for key in list(json_loaded_response.keys()):
+                        if(key != 'name'):
+                            json_loaded_response.pop(key)
+                    json_loaded_response = list(json_loaded_response.keys())
+                except:
+                    pass
+        
         return json_loaded_response
+    
+    
+    
+    
+
 
 
