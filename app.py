92a164d3e2e049c6fb7a7384ee80918f3c2e66de
ArjavTripathi <arjavtri@iastate.edu>
2025-02-15 19:12:18 -0600

    First

--- app.py
+++ app.py
@@ -10,9 +10,13 @@ STEAM_API_KEY = os.environ.get("API_KEY")
 
 @app.route('/api/user/<steam_id>')
 def get_user_info(steam_id):
-    print(steam_id)
-    print(STEAM_API_KEY)
-    return jsonify({'success': '555'})
+    url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_API_KEY}&steamid={steam_id}'
+    print(url)
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return jsonify(resp.json())
+    else:
+        return jsonify({'error': 'Failed to fetch user data'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
