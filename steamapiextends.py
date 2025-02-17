b68be612a882517aeddbcd9bcfd0175e92526e88
ArjavTripathi <arjavtri@iastate.edu>
2025-02-16 22:38:21 -0600

    IgnoredExtraFields

--- steamapiextends.py
+++ steamapiextends.py
@@ -17,9 +17,65 @@ class extendsAPI(Apps):
             self.__app_details_url,
             params={"appids": app_id, "cc": country, "filters": filters},
         )
-        split : list[str] = filters.split(',')
         json_loaded_response = json.loads(response.text)
+        split : list[str] = filters.split(',')
         if(json_loaded_response[f'{app_id}']['success']):
+            try:
+                del json_loaded_response[f'{app_id}']['data']['type']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['pc_requirements']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['legal_notice']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['about_the_game']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['detailed_description']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['drm_notice']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['capsule_imagev5']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['capsule_image']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['required_age']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['linux_requirements']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['mac_requirements']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['reviews']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['short_description']
+            except:
+                pass
+            try:
+                del json_loaded_response[f'{app_id}']['data']['supported_languages']
+            except:
+                pass
             if('categories' in split):
                 try:
                     json_loaded_response[f'{app_id}']['data']['categories'] = [category['description'] for category in json_loaded_response[f'{app_id}']['data']['categories'] if category['description'] in {'Single-player', 'Multi-player', 'Co-op', 'Online Co-op', 'Full controller support', 'Family Sharing', 'Partial Controller Support', 'In-App Purchases', 'Cross-Platform Multiplayer', 'Shared/Split Screen', 'Remote Play on TV', 'Remote Play Together', 'VR Supported'}]
@@ -35,6 +91,13 @@ class extendsAPI(Apps):
                     json_loaded_response = list(json_loaded_response.keys())
                 except:
                     pass
+            if('price_overview' in split and json_loaded_response[f'{app_id}']['data']['is_free']):
+                try:
+                    del json_loaded_response[f'{app_id}']['data']['price_overview']
+                except:
+                    pass
+
+
         
         return json_loaded_response
     
